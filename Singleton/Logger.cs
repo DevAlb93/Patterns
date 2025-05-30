e5f74240520542d78008c72e48d5a71637ccf036
jurik <jurik@JURGEN>
2025-05-30 16:57:07 +0200

    Add of Singleton Pattern

---
+++ Singleton/Logger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Logger
+    {
+        //Thread Safe with Lazy<T> during initialization
+        public static readonly Lazy<Logger> LazyInstance =
+            new(() => new Logger());
+        private Logger()
+        {
+        }
+
+        public static Logger GetInstance => LazyInstance.Value;
+
+        //private static Logger? _instance;
+        //public static Logger GetInstance() 
+        //{
+        //    if (_instance == null)
+        //    {
+        //        _instance = new Logger();
+        //    }
+
+        //    return _instance;
+        //}
+
+        ///SingletonOperation
+        ///
+        public void Log(string message)
+        {
+            Console.WriteLine($"Log: {message}");
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Singleton;
+
+Console.WriteLine("Testing SingletonPattern");
+
+var singleton = Logger.GetInstance;
+singleton.Log("This is a log message from the singleton instance.");
+Console.ReadLine(); 
